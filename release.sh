4a95294312b395ef2d1bf9a702acae8740726357
microwavedcola1 <microwavedcola@gmail.com>
2022-02-23 11:12:14 +0100

    add todo, fix program id
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- release.sh
+++ release.sh
@@ -18,12 +18,12 @@ anchor build
 # update on chain program and IDL, atm used for testing/developing
 anchor deploy --provider.cluster devnet --provider.wallet ${PROVIDER_WALLET}
 anchor idl upgrade --provider.cluster devnet --provider.wallet ${PROVIDER_WALLET}\
- --filepath target/idl/voter_stake_registry.json 4Q6WW2ouZ6V3iaNm56MTd5n2tnTm4C5fiH8miFHnAFHo
+ --filepath target/idl/voter_stake_registry.json Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS
 
 # update types in npm package and publish the npm package
 cp cp ./target/types/mango_v4.ts ./src/mango_v4.ts
-# yarn clean && yarn build && cp package.json ./dist/
-# yarn publish dist
+yarn clean && yarn build && cp package.json ./dist/
+# yarn publish dist # TODO: should this package replace mango-v3-client?
 
 echo
 echo Remember to commit and push the version update as well as the changes
