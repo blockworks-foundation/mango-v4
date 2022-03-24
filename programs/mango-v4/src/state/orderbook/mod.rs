baf4b255c8e4523aca2ed7b710aa86444a11ecd0
microwavedcola1 <microwavedcola@gmail.com>
2022-03-24 14:48:11 +0100

    remove more dead code
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/mango-v4/src/state/orderbook/mod.rs
+++ programs/mango-v4/src/state/orderbook/mod.rs
@@ -1,8 +1,6 @@
 pub use book::*;
 pub use bookside::*;
 pub use bookside_iterator::*;
-pub use datatype::*;
-pub use metadata::*;
 pub use nodes::*;
 pub use order_type::*;
 pub use queue::*;
@@ -10,8 +8,6 @@ pub use queue::*;
 pub mod book;
 pub mod bookside;
 pub mod bookside_iterator;
-pub mod datatype;
-pub mod metadata;
 pub mod nodes;
 pub mod order_type;
 pub mod queue;
