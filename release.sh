095ad30ac4e43e87d1dd80ef74c4d669b079e595
microwavedcola1 <microwavedcola@gmail.com>
2022-03-31 21:03:45 +0200

    release script
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- release.sh
+++ release.sh
@@ -2,31 +2,31 @@
 
 set -euo pipefail
 
-# build program, TODO try removing --skip-lint
+WALLET_WITH_FUNDS=~/.config/solana/mango-devnet.json
+PROGRAM_ID=m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD
+
+# TODO fix need for --skip-lint
+# build program, 
 anchor build --skip-lint
+
+# patch types, which we want in rust, but anchor client doesn't support
 ./idl-fixup.sh
 
 # update types in ts client package
 cp -v ./target/types/mango_v4.ts ./ts/mango_v4.ts
-yarn clean && yarn build && cp package.json ./dist/
 
-# if [[ -z "${PROVIDER_WALLET}" ]]; then
-#   echo "Please provide path to a provider wallet keypair."
-#   exit -1
-# fi
+# publish program
+solana --url https://mango.devnet.rpcpool.com program deploy --program-id $PROGRAM_ID  \
+    -k $WALLET_WITH_FUNDS target/deploy/mango_v4.so
 
-# if [[ -z "${VERSION_MANUALLY_BUMPED}" ]]; then
-#   echo "Please bump versions in package.json and in cargo.toml."
-#   exit -1
-# fi
+# publish idl
+anchor idl init --provider.cluster https://mango.devnet.rpcpool.com --provider.wallet $WALLET_WITH_FUNDS \
+    --filepath target/idl/mango_v4.json $PROGRAM_ID
 
-# # update on chain program and IDL, atm used for testing/developing
-# anchor deploy --provider.cluster devnet --provider.wallet ${PROVIDER_WALLET}
-# anchor idl upgrade --provider.cluster devnet --provider.wallet ${PROVIDER_WALLET}\
-#  --filepath target/idl/voter_stake_registry.json m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD
+# build npm package
+# yarn clean && yarn build && cp package.json ./dist/
 
-
-# # publish the npm package
+# publish the npm package
 # yarn publish dist
 
 # echo
