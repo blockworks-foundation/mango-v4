4e649a2ee255f2c11630c71ea8541b57690c5b04
microwavedcola1 <microwavedcola@gmail.com>
2022-05-12 08:44:04 +0200

    remove unused
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- ts/client/src/constants.ts
+++ ts/client/src/constants.ts
@@ -1,7 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const DEVNET_GROUP = 'NDLmPdjzm7Tm3gnsZw8EoBbBTR7wfAcyDY8LCqFSGtX';
-
 export const DEVNET_SERUM3_PROGRAM_ID = new PublicKey(
   'DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY',
 );
