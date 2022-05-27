b6ac1afb72390e590294a8691dfb1c6042710e31
Christian Kamm <mail@ckamm.de>
2022-05-27 09:23:17 +0200

    CreateGroup: Allow multiple groups per admin

--- programs/mango-v4/src/instructions/create_group.rs
+++ programs/mango-v4/src/instructions/create_group.rs
@@ -4,10 +4,11 @@ use crate::error::*;
 use crate::state::*;
 
 #[derive(Accounts)]
+#[instruction(group_num: u32)]
 pub struct CreateGroup<'info> {
     #[account(
         init,
-        seeds = [b"Group".as_ref(), admin.key().as_ref()],
+        seeds = [b"Group".as_ref(), admin.key().as_ref(), &group_num.to_le_bytes()],
         bump,
         payer = payer,
         space = 8 + std::mem::size_of::<Group>(),
@@ -22,9 +23,10 @@ pub struct CreateGroup<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn create_group(ctx: Context<CreateGroup>) -> Result<()> {
+pub fn create_group(ctx: Context<CreateGroup>, group_num: u32) -> Result<()> {
     let mut group = ctx.accounts.group.load_init()?;
     group.admin = ctx.accounts.admin.key();
     group.bump = *ctx.bumps.get("group").ok_or(MangoError::SomeError)?;
+    group.group_num = group_num;
     Ok(())
 }
