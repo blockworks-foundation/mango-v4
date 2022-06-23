c1dea82a55fa3abdc549ec84727537647403cf63
microwavedcola1 <89031858+microwavedcola1@users.noreply.github.com>
2022-06-23 15:22:59 +0200

    mainnet scripts (#80)
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

---
+++ release-to-mainnet.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e pipefail
+
+WALLET_WITH_FUNDS=~/.config/solana/mango-mainnet.json
+PROGRAM_ID=m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD
+
+# TODO fix need for --skip-lint
+# build program, 
+anchor build --skip-lint
+
+# patch types, which we want in rust, but anchor client doesn't support
+./idl-fixup.sh
+
+# update types in ts client package
+cp -v ./target/types/mango_v4.ts ./ts/client/src/mango_v4.ts
+
+(cd ./ts/client && tsc)
+
+if [[ -z "${NO_DEPLOY}" ]]; then
+    # publish program
+    solana --url $CLUSTER_URL program deploy --program-id $PROGRAM_ID  \
+        -k $WALLET_WITH_FUNDS target/deploy/mango_v4.so
+
+    # publish idl
+    anchor idl upgrade --provider.cluster $CLUSTER_URL --provider.wallet $WALLET_WITH_FUNDS \
+        --filepath target/idl/mango_v4.json $PROGRAM_ID
+else
+    echo "Skipping deployment..."
+fi
+
+
+# build npm package
+(cd ./ts/client && tsc)
--- ts/client/src/constants/index.ts
+++ ts/client/src/constants/index.ts
@@ -1,13 +1,13 @@
 import { PublicKey } from '@solana/web3.js';
 
 export const SERUM3_PROGRAM_ID = {
-  testnet: new PublicKey('DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY'), // todo: fix
+  testnet: new PublicKey('DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY'),
   devnet: new PublicKey('DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY'),
-  mainnet: new PublicKey('9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'),
+  'mainnet-beta': new PublicKey('9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'),
 };
 
 export const MANGO_V4_ID = {
-  testnet: new PublicKey('m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD'), // todo: fix
+  testnet: new PublicKey('m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD'),
   devnet: new PublicKey('m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD'),
-  mainnet: new PublicKey('m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD'), // note: never deployed so far
+  'mainnet-beta': new PublicKey('m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD'),
 };
