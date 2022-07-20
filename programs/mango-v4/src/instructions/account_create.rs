3a411204144c3f45f52d3210332a985b94f12dce
Christian Kamm <mail@ckamm.de>
2022-07-20 12:23:01 +0200

    fixup: use anchor's init annotation

--- programs/mango-v4/src/instructions/account_create.rs
+++ programs/mango-v4/src/instructions/account_create.rs
@@ -27,9 +27,11 @@ pub struct AccountCreate<'info> {
     pub system_program: Program<'info, System>,
 
     #[account(
-        mut,
+        init,
         seeds = [group.key().as_ref(), b"MangoAccount2".as_ref(), owner.key().as_ref(), &account_num.to_le_bytes()],
-        bump
+        bump,
+        payer = payer,
+        space = MangoAccount2::space(3, 4, 2),
     )]
     // borsh smashes the stack, and zero copy doesnt work out of the box
     // deserialize manually
@@ -54,50 +56,17 @@ pub fn account_create(ctx: Context<AccountCreate>, account_num: u8, name: String
     //
     // mango account 2 i.e. mango account with expandable positions
     //
-    let token_count: usize = 3;
-    let serum3_count: usize = 4;
-    let perp_count: usize = 2;
-    // create account
-    {
-        let space = MangoAccount2::space(token_count, serum3_count, perp_count);
-        let lamports = Rent::get()?.minimum_balance(space);
-        let cpi_accounts = anchor_lang::system_program::CreateAccount {
-            from: ctx.accounts.payer.to_account_info(),
-            to: ctx.accounts.account2.to_account_info(),
-        };
-        let cpi_context = anchor_lang::context::CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            cpi_accounts,
-        );
-        let (__pda_address, __bump) = Pubkey::find_program_address(
-            &[
-                ctx.accounts.group.key().as_ref(),
-                b"MangoAccount2".as_ref(),
-                ctx.accounts.owner.key().as_ref(),
-                &account_num.to_le_bytes(),
-            ],
-            ctx.program_id,
-        );
-        anchor_lang::system_program::create_account(
-            cpi_context.with_signer(&[&[
-                ctx.accounts.group.key().as_ref(),
-                b"MangoAccount2".as_ref(),
-                ctx.accounts.owner.key().as_ref(),
-                &account_num.to_le_bytes(),
-                &[__bump][..],
-            ][..]]),
-            lamports,
-            space as u64,
-            ctx.program_id,
-        )?;
-    }
-    // init disc, dynamic fields lengths
+    // init disc
     let mal: MangoAccountLoader<MangoAccount2Fixed, MangoAccount2DynamicHeader, MangoAccount2> =
         MangoAccountLoader::new(ctx.accounts.account2.to_account_info())?;
     let mut meta = mal.load_init()?;
     // init fixed fields
     // later we would expand, and verify if the existing ones are set and new expanded ones are unset
     meta.fixed.owner = ctx.accounts.owner.key();
+    // init dynamic fields
+    let token_count: usize = 3;
+    let serum3_count: usize = 4;
+    let perp_count: usize = 2;
     meta.dynamic
         .expand_dynamic_content(token_count, serum3_count, perp_count)?;
     // test
