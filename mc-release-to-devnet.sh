18a50676ec9e748b04e829f20b14aa5c91e4218a
microwavedcola1 <microwavedcola@gmail.com>
2022-07-25 15:53:21 +0200

    fix script
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- mc-release-to-devnet.sh
+++ mc-release-to-devnet.sh
@@ -9,10 +9,10 @@ if [ "$ANCHOR_FORK" != "$ANCHOR_BRANCH" ]; then
   exit 1;
 fi
 
-# rg m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD -l | xargs -I % sed -i '' 's/m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD/m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD/g' %;
+# rg m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD -l | xargs -I % sed -i '' 's/m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD/5V2zCYCQkm4sZc3WctiwQEAzvfAiFxyjbwCvzQnmtmkM/g' %;
 
 WALLET_WITH_FUNDS=~/.config/solana/mango-devnet.json
-PROGRAM_ID=m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD
+PROGRAM_ID=5V2zCYCQkm4sZc3WctiwQEAzvfAiFxyjbwCvzQnmtmkM
 
 # TODO fix need for --skip-lint
 # build program, 
@@ -26,7 +26,7 @@ cp -v ./target/types/mango_v4.ts ./ts/client/src/mango_v4.ts
 
 if [[ -z "${NO_DEPLOY}" ]]; then
     # publish program
-    solana --url https://mango.devnet.rpcpool.com program deploy --program-id m43thNJ58XCjL798ZSq6JGAG1BnWskhdq5or6kcnfsD -k ~/.config/solana/mango-devnet.json target/deploy/mango_v4.so
+    solana --url https://mango.devnet.rpcpool.com program deploy --program-id 5V2zCYCQkm4sZc3WctiwQEAzvfAiFxyjbwCvzQnmtmkM -k ~/.config/solana/mango-devnet.json target/deploy/mango_v4.so
 
     # publish idl
     cargo run --manifest-path ../anchor/cli/Cargo.toml idl upgrade --provider.cluster https://mango.devnet.rpcpool.com --provider.wallet $WALLET_WITH_FUNDS --filepath target/idl/mango_v4.json $PROGRAM_ID
