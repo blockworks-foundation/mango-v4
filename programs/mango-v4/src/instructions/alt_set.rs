fc47b35d65a92d8821494064fe2603170152fcbf
Christian Kamm <mail@ckamm.de>
2022-09-14 10:55:24 +0200

    Add alt_set / alt_extend instructions
    alt_extend will only work in the future, when alt_set can receive lookup
    tables where the authority is set to be the mango group pda.

---
+++ programs/mango-v4/src/instructions/alt_set.rs
@@ -0,0 +1,46 @@
+use anchor_lang::prelude::*;
+use solana_address_lookup_table_program as solana_alt;
+
+use crate::error::*;
+use crate::state::*;
+
+#[derive(Accounts)]
+pub struct AltSet<'info> {
+    #[account(
+        mut,
+        has_one = admin,
+    )]
+    pub group: AccountLoader<'info, Group>,
+    pub admin: Signer<'info>,
+
+    /// CHECK: ALT authority is checked inline
+    #[account(
+        mut,
+        owner = solana_alt::ID,
+    )]
+    pub address_lookup_table: UncheckedAccount<'info>,
+}
+
+/// Register or replace an address lookup table
+pub fn alt_set(ctx: Context<AltSet>, index: u8) -> Result<()> {
+    let alt_bytes = ctx.accounts.address_lookup_table.try_borrow_data()?;
+    solana_alt::state::AddressLookupTable::deserialize(&alt_bytes)
+        .map_err(|e| error_msg!("could not deserialize alt: {}", e))?;
+
+    // FUTURE: When the solana feature
+    //   relax_authority_signer_check_for_lookup_table_creation
+    //   "relax authority signer check for lookup table creation #27205"
+    // is enabled (introduced in b79abb4fab62da487c6834926ef309d4c1b69011)
+    // we can require ALTs to have the group as owner.
+    /*
+    require_msg!(
+        alt_data.meta.authority.is_some(),
+        "alt must have an authority"
+    );
+    require_keys_eq!(alt_data.meta.authority.unwrap(), ctx.accounts.group.key());
+    */
+
+    let mut group = ctx.accounts.group.load_mut()?;
+    group.address_lookup_tables[index as usize] = ctx.accounts.address_lookup_table.key();
+    Ok(())
+}
