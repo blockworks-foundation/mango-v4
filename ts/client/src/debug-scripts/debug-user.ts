6e8f6596e737350d9e9f81b184cf2142bbcadf33
microwavedcola1 <microwavedcola@gmail.com>
2022-12-19 14:46:39 +0100

    rename
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- ts/client/src/debug-scripts/debug-user.ts
+++ ts/client/src/debug-scripts/debug-user.ts
@@ -17,7 +17,7 @@ const PAYER_KEYPAIR =
 const USER_KEYPAIR =
   process.env.USER_KEYPAIR_OVERRIDE || process.env.MB_PAYER_KEYPAIR;
 const GROUP_NUM = Number(process.env.GROUP_NUM || 0);
-const MANGO_ACCOUNT = process.env.MANGO_ACCOUNT;
+const MANGO_ACCOUNT = process.env.MANGO_ACCOUNT_PK;
 const CLUSTER: Cluster =
   (process.env.CLUSTER_OVERRIDE as Cluster) || 'mainnet-beta';
 
