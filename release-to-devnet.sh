edd0633489e55c81b4c16f34c41059e14388488a
microwavedcola1 <microwavedcola@gmail.com>
2022-12-19 15:38:06 +0100

    change to new authority
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- release-to-devnet.sh
+++ release-to-devnet.sh
@@ -2,7 +2,7 @@
 
 set -ex pipefail
 
-WALLET_WITH_FUNDS=~/.config/solana/mango-devnet.json
+WALLET_WITH_FUNDS=~/.config/solana/mango-mainnet-1.json
 PROGRAM_ID=4MangoMjqJ2firMokCjjGgoK8d4MXcrgL7XJaL3w6fVg
 
 # build program, 
@@ -20,7 +20,7 @@ cp -v ./target/types/mango_v4.ts ./ts/client/src/mango_v4.ts
 solana --url https://mango.devnet.rpcpool.com program deploy --program-id $PROGRAM_ID  \
     -k $WALLET_WITH_FUNDS target/deploy/mango_v4.so --skip-fee-check
 
-# # publish idl
+# publish idl
 cargo run -p anchor-cli -- idl upgrade --provider.cluster https://mango.devnet.rpcpool.com --provider.wallet $WALLET_WITH_FUNDS \
     --filepath target/idl/mango_v4_no_docs.json $PROGRAM_ID
 
