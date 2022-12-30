7a8f31c6118b0577aa6375af4643e41efe85e057
Christian Kamm <mail@ckamm.de>
2022-12-30 09:54:31 +0100

    Perp: settlement applies no loan origination fee
    It's likely for perp pnl to oscillate and be settled in positive and
    negative direction repeatedly. If the user's USDC balance is <= 0, every
    settlement of negative pnl will increase borrows and cause loan
    origination fees.
    
    Over time the fees accumulated this way could be significant. Skipping
    loan origination fees for borrows created from perp settlement avoids
    that.

--- programs/mango-v4/src/instructions/perp_settle_fees.rs
+++ programs/mango-v4/src/instructions/perp_settle_fees.rs
@@ -104,7 +104,7 @@ pub fn perp_settle_fees(ctx: Context<PerpSettleFees>, max_settle_amount: u64) ->
     let token_position = account
         .token_position_mut(perp_market.settle_token_index)?
         .0;
-    bank.withdraw_with_fee(
+    bank.withdraw_without_fee(
         token_position,
         settlement,
         Clock::get()?.unix_timestamp.try_into().unwrap(),
