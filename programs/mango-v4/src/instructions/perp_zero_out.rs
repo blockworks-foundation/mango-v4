76e177ccd413557cf8febf45bf3de3a6135e7f8d
microwavedcola1 <microwavedcola@gmail.com>
2023-01-21 14:00:48 +0100

    fix
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/mango-v4/src/instructions/perp_zero_out.rs
+++ programs/mango-v4/src/instructions/perp_zero_out.rs
@@ -29,19 +29,20 @@ pub fn perp_zero_out_for_market(ctx: Context<PerpZeroOutForMarket>) -> Result<()
     let mut account = ctx.accounts.account.load_full_mut()?;
 
     let perp_market = ctx.accounts.perp_market.load()?;
+    
     let mut perp_position = account.perp_position_mut(perp_market.perp_market_index)?;
     *perp_position = PerpPosition::zeroed();
     perp_position.market_index = PerpMarketIndex::MAX;
 
     for i in 0..account.header.perp_oo_count() {
-        let mut oo = account.perp_order_by_raw_index(i);
+        let mut oo = account.perp_order_mut_by_raw_index(i);
         if !oo.is_active_for_market(perp_market.perp_market_index) {
             continue;
         }
-        *oo.market = FREE_ORDER_SLOT;
-        *oo.side_and_tree = SideAndOrderTree::BidFixed.into();
-        *oo.id = 0;
-        *oo.client_id = 0;
+        oo.market = FREE_ORDER_SLOT;
+        oo.side_and_tree = SideAndOrderTree::BidFixed.into();
+        oo.id = 0;
+        oo.client_id = 0;
     }
 
     Ok(())
