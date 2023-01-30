3ad0b39926f9060c657f2d6a43f0631731e75f13
microwavedcola1 <microwavedcola@gmail.com>
2023-01-30 14:37:01 +0100

    add export
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- ts/client/src/index.ts
+++ ts/client/src/index.ts
@@ -4,7 +4,6 @@ import { MangoClient } from './client';
 import { MANGO_V4_ID } from './constants';
 
 export * from './accounts/bank';
-export * from './numbers/I80F48';
 export * from './accounts/mangoAccount';
 export * from './accounts/perp';
 export {
@@ -13,6 +12,8 @@ export {
   Serum3SelfTradeBehavior,
   Serum3Side,
 } from './accounts/serum3';
+export * from './clientIxParamBuilder';
 export * from './constants';
+export * from './numbers/I80F48';
 export * from './utils';
 export { Group, StubOracle, MangoClient, MANGO_V4_ID };
