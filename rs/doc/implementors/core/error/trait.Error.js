e944af189239532898bf1bcb7782242f334134d8
ckamm <mail@ckamm.de>
2023-02-20 11:25:21 +0000

    Deploying to gh-pages from @ blockworks-foundation/mango-v4@7a51ce0619cda60e30313b45c8bfb02426c29ebc 🚀

--- rs/doc/implementors/core/error/trait.Error.js
+++ rs/doc/implementors/core/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"client":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"client/enum.MangoClientError.html\" title=\"enum client::MangoClientError\">MangoClientError</a>"]]
+"client":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"client/enum.MangoClientError.html\" title=\"enum client::MangoClientError\">MangoClientError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
