f29fffd3314dd6a6522940d9b60b342fd0ec18c1
Christian Kamm <mail@ckamm.de>
2023-03-03 09:53:27 +0100

    Perp: User-selected reduce-only behaves more intuitively (#483)
    The previous strict behavior of taking existing open orders into account
    is only used when the perp market itself is also flagged as reduce-only.

--- programs/mango-v4/src/state/mango_account_components.rs
+++ programs/mango-v4/src/state/mango_account_components.rs
@@ -179,10 +179,10 @@ pub struct PerpPosition {
     pub settle_pnl_limit_settled_in_current_window_native: i64,
 
     /// Active position size, measured in base lots
-    base_position_lots: i64,
+    pub base_position_lots: i64,
     /// Active position in quote (conversation rate is that of the time the order was settled)
     /// measured in native quote
-    quote_position_native: I80F48,
+    pub quote_position_native: I80F48,
 
     /// Tracks what the position is to calculate average entry & break even price
     pub quote_running_native: i64,
