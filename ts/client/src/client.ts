3600b6592cb317a4bba369d7fa16d8bc702ad1f3
Christian Kamm <mail@ckamm.de>
2023-05-05 09:11:47 +0200

    ts-client: Fix serum taker fee computation (#573)

--- ts/client/src/client.ts
+++ ts/client/src/client.ts
@@ -1612,12 +1612,12 @@ export class MangoClient {
     const limitPrice = serum3MarketExternal.priceNumberToLots(price);
     const maxBaseQuantity = serum3MarketExternal.baseSizeNumberToLots(size);
     const isTaker = orderType !== Serum3OrderType.postOnly;
-    const maxQuoteQuantity = new BN(
+    const maxQuoteQuantity = new BN(Math.ceil(
       serum3MarketExternal.decoded.quoteLotSize.toNumber() *
         (1 + Math.max(serum3Market.getFeeRates(isTaker), 0)) *
         serum3MarketExternal.baseSizeNumberToLots(size).toNumber() *
         serum3MarketExternal.priceNumberToLots(price).toNumber(),
-    );
+    ));
 
     const payerTokenIndex = ((): TokenIndex => {
       if (side == Serum3Side.bid) {
