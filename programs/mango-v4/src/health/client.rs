bb2bd5ffc37f4ffcdbca34f16e780731f24dea3b
Maximilian Schneider <mail@maximilianschneider.net>
2023-07-11 00:21:40 +0200

    add spot oo test & fmt

--- programs/mango-v4/src/health/client.rs
+++ programs/mango-v4/src/health/client.rs
@@ -449,7 +449,7 @@ impl HealthCache {
 
     /// Computes the account equity and the summarized balance sheet marked to market.
     /// Returns (equity, assets, liabilities)
-    pub fn equity(&self) ->  (I80F48, I80F48, I80F48) {
+    pub fn equity(&self) -> (I80F48, I80F48, I80F48) {
         let mut assets = I80F48::ZERO;
         let mut liabs = I80F48::ZERO;
 
@@ -486,7 +486,7 @@ impl HealthCache {
         }
 
         let equity = assets - liabs;
-        return (equity, assets, liabs)
+        return (equity, assets, liabs);
     }
 
     /// Computes the account leverage as ratio of liabs / (assets - liabs)
@@ -1593,7 +1593,7 @@ mod tests {
         };
         assert!(leverage_eq(&health_cache, 9.9));
 
-        // open orders: assets = , equity =
+        // open orders: assets = 3, equity = 1
         let health_cache = HealthCache {
             token_infos: vec![
                 TokenInfo {
@@ -1603,27 +1603,22 @@ mod tests {
                 },
                 TokenInfo {
                     token_index: 1,
+                    balance_spot: I80F48::from_num(-1),
                     ..default_token_info(0.2, 2.0)
                 },
             ],
             serum3_infos: vec![Serum3Info {
-                reserved_base: I80F48::ZERO,
+                reserved_base: I80F48::ONE,
                 reserved_quote: I80F48::ZERO,
                 base_info_index: 1,
                 quote_info_index: 0,
                 market_index: 0,
                 has_zero_funds: true,
             }],
-            perp_infos: vec![PerpInfo {
-                perp_market_index: 0,
-                base_lot_size: 3,
-                base_lots: -1,
-                quote: I80F48::from_num(3.3),
-                ..default_perp_info(0.1, 1.1)
-            }],
+            perp_infos: vec![],
             being_liquidated: false,
         };
 
-        // assert_eq!(health_cache.leverage(), I80F48::ZERO);
+        assert!(leverage_eq(&health_cache, 2.0));
     }
 }
