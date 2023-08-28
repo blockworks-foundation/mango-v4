33717ef2442176f16d1d37b8f4b59f8df6615b2e
Christian Kamm <mail@ckamm.de>
2023-08-28 11:58:04 +0200

    FlashLoan: fee fixes
    - Rename the new "swap fee" to "deposit fee" and let it apply to all
      deposits, not just for Swap-type flash loans.
    - But don't apply it to withdrawals (effectively giving rebates!)
    
    Result of audit feedback

--- ts/client/src/accounts/bank.ts
+++ ts/client/src/accounts/bank.ts
@@ -124,7 +124,7 @@ export class Bank implements BankForHealth {
       feesWithdrawn: BN;
       tokenConditionalSwapTakerFeeRate: number;
       tokenConditionalSwapMakerFeeRate: number;
-      flashLoanSwapFeeRate: number;
+      flashLoanDepositFeeRate: number;
     },
   ): Bank {
     return new Bank(
@@ -175,7 +175,7 @@ export class Bank implements BankForHealth {
       obj.feesWithdrawn,
       obj.tokenConditionalSwapTakerFeeRate,
       obj.tokenConditionalSwapMakerFeeRate,
-      obj.flashLoanSwapFeeRate,
+      obj.flashLoanDepositFeeRate,
     );
   }
 
@@ -227,7 +227,7 @@ export class Bank implements BankForHealth {
     public feesWithdrawn: BN,
     public tokenConditionalSwapTakerFeeRate: number,
     public tokenConditionalSwapMakerFeeRate: number,
-    public flashLoanSwapFeeRate: number,
+    public flashLoanDepositFeeRate: number,
   ) {
     this.name = utf8.decode(new Uint8Array(name)).split('\x00')[0];
     this.oracleConfig = {
