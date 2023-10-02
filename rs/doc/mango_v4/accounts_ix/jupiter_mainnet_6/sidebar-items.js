aa41d9950015b14b32b5312052f0daf1d621bcb6
ckamm <mail@ckamm.de>
2023-10-02 18:07:52 +0000

    Deploying to gh-pages from @ blockworks-foundation/mango-v4@1d241de75ade649e7e661145fca02a6fb3dea999 🚀

---
+++ rs/doc/mango_v4/accounts_ix/jupiter_mainnet_6/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["check_id","Confirms that a given pubkey is equivalent to the program ID"],["id","Returns the program ID"]],"static":[["ID","The static program ID"]]};
