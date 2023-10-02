e1d6fbbed6cb5b8a8380e82964554979ef0c0065
Christian Kamm <mail@ckamm.de>
2023-10-02 20:10:42 +0200

    Changelog and version bump for v0.20.0

--- ts/client/src/mango_v4.ts
+++ ts/client/src/mango_v4.ts
@@ -1,5 +1,5 @@
 export type MangoV4 = {
-  "version": "0.19.1",
+  "version": "0.20.0",
   "name": "mango_v4",
   "instructions": [
     {
@@ -12322,7 +12322,7 @@ export type MangoV4 = {
 };
 
 export const IDL: MangoV4 = {
-  "version": "0.19.1",
+  "version": "0.20.0",
   "name": "mango_v4",
   "instructions": [
     {
