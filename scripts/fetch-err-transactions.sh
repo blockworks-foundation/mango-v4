37d1bd151bbf8c201659fe883bb90e0ccebc0ee7
microwavedcola1 <microwavedcola@gmail.com>
2023-10-15 12:20:15 +0200

    script: error tx grouping, blacklist some more
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- scripts/fetch-err-transactions.sh
+++ scripts/fetch-err-transactions.sh
@@ -18,13 +18,14 @@ mkdir -p err-txs/processed
 rm -rf err-txs/processed/*
 
 # File containing last n number of error signatures, fetch txs
-cat ~/Downloads/err-tx-sigs | parallel -k -j8 "curl -sS -X POST $RPC_URL -H 'Content-type: application/json' -d'$REQUEST_BODY' > err-txs/original/{}"
+# cat ~/Downloads/err-tx-sigs | parallel -k -j8 "curl -sS -X POST $RPC_URL -H 'Content-type: application/json' -d'$REQUEST_BODY' > err-txs/original/{}"
 
 # Ignore market makers
 cp err-txs/original/* err-txs/bak/
 grep -rl '4hXPGTmR6dKNNqjLYdfDRSrTaa1Wt2GZoZnQ9hAJEeev' err-txs/bak | xargs rm
 grep -rl 'BLgb4NFwhpurMrGX5LQfb8D8dBpGSGtBqqew2Em8uyRT' err-txs/bak | xargs rm
 grep -rl '2f4nvyfS47tL8XaMt1Nm8kiE6dPW2W5udoRSWZch1bK9' err-txs/bak | xargs rm
+
 
 # Extract logs for easy viewing
 find err-txs/bak/* | xargs -I % basename % | parallel "jq '.result.meta.logMessages' err-txs/bak/{} > err-txs/processed/{}"
@@ -37,6 +38,12 @@ grep -rl 'programs/dyson/src/instructions/swap_protected' err-txs/processed | xa
 grep -rl 'SlippageToleranceExceeded' err-txs/processed | xargs rm
 grep -rl 'InvalidCalculation' err-txs/processed | xargs rm
 grep -rl 'RaydiumSwapExactOutput' err-txs/processed | xargs rm
+grep -rl 'RaydiumClmmSwapExactOutput' err-txs/processed | xargs rm
+grep -rl 'SharedAccountsExactOutRoute' err-txs/processed | xargs rm
 grep -rl 'OracleStale' err-txs/processed | xargs rm
+grep -rl 'AccountCreate' err-txs/processed | xargs rm
+grep -rl 'ProfitabilityMismatch' err-txs/processed | xargs rm
+grep -rl 'token is in reduce only mode' err-txs/processed | xargs rm
+
 
 find err-txs/processed/ -size 0 -print -delete
