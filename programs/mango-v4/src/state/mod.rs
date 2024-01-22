8a94d28d20d9cf8b7a7cce06199fc1935855799f
Lou-Kamades <lou_kamades@proton.me>
2024-01-22 13:37:53 -0600

    add RaydiumCLMM oracle type

--- programs/mango-v4/src/state/mod.rs
+++ programs/mango-v4/src/state/mod.rs
@@ -1,3 +1,4 @@
+pub use orca_cpi::*;
 pub use bank::*;
 pub use dynamic_account::*;
 pub use equity::*;
@@ -7,13 +8,13 @@ pub use mango_account_components::*;
 pub use mint_info::*;
 pub use openbook_v2_market::*;
 pub use oracle::*;
-pub use orca_cpi::*;
 pub use orderbook::*;
 pub use perp_market::*;
 pub use serum3_market::*;
 pub use stable_price::*;
 pub use token_conditional_swap::*;
 
+mod orca_cpi;
 mod bank;
 mod dynamic_account;
 mod equity;
@@ -23,7 +24,6 @@ mod mango_account_components;
 mod mint_info;
 mod openbook_v2_market;
 mod oracle;
-mod orca_cpi;
 mod orderbook;
 mod perp_market;
 mod serum3_market;
