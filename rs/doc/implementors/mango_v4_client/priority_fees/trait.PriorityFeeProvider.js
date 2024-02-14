7fe88311b1a446ae04cbc0c68fb71c345a85facd
ckamm <mail@ckamm.de>
2024-02-14 09:13:28 +0000

    Deploying to gh-pages from @ blockworks-foundation/mango-v4@4f5ec41d7ad8480a262c384a93cfe0ac4daca33f 🚀

---
+++ rs/doc/implementors/mango_v4_client/priority_fees/trait.PriorityFeeProvider.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mango_v4_client":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
