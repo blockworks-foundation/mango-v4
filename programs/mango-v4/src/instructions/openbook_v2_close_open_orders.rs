690e762e2f8326ac0ee035d32787c53dd9c7bb01
Riordan Panayides <riordan@panayid.es>
2024-02-29 15:20:13 +0000

    fix close open orders

--- programs/mango-v4/src/instructions/openbook_v2_close_open_orders.rs
+++ programs/mango-v4/src/instructions/openbook_v2_close_open_orders.rs
@@ -7,53 +7,75 @@ use crate::error::MangoError;
 use crate::state::*;
 
 pub fn openbook_v2_close_open_orders(ctx: Context<OpenbookV2CloseOpenOrders>) -> Result<()> {
+    let openbook_market = ctx.accounts.openbook_v2_market.load()?;
+
     //
     // Validation
     //
-    let mut account = ctx.accounts.account.load_full_mut()?;
-    // account constraint #1
-    require!(
-        account
-            .fixed
-            .is_owner_or_delegate(ctx.accounts.authority.key()),
-        MangoError::SomeError
-    );
+    {
+        let account = ctx.accounts.account.load_full()?;
+        // account constraint #1
+        require!(
+            account
+                .fixed
+                .is_owner_or_delegate(ctx.accounts.authority.key()),
+            MangoError::SomeError
+        );
 
-    let openbook_market = ctx.accounts.openbook_v2_market.load()?;
+        // Validate open_orders #2
+        require!(
+            account
+                .openbook_v2_orders(openbook_market.market_index)?
+                .open_orders
+                == ctx.accounts.open_orders_account.key(),
+            MangoError::SomeError
+        );
 
-    // Validate open_orders #2
-    require!(
-        account
-            .openbook_v2_orders(openbook_market.market_index)?
-            .open_orders
-            == ctx.accounts.open_orders_account.key(),
-        MangoError::SomeError
-    );
-
-    // Validate banks #3
-    let mut quote_bank = ctx.accounts.quote_bank.load_mut()?;
-    let mut base_bank = ctx.accounts.base_bank.load_mut()?;
-    require_eq!(
-        quote_bank.token_index,
-        openbook_market.quote_token_index,
-        MangoError::SomeError
-    );
-    require_eq!(
-        base_bank.token_index,
-        openbook_market.base_token_index,
-        MangoError::SomeError
-    );
-
+        // Validate banks #3
+        let quote_bank = ctx.accounts.quote_bank.load()?;
+        let base_bank = ctx.accounts.base_bank.load()?;
+        require_eq!(
+            quote_bank.token_index,
+            openbook_market.quote_token_index,
+            MangoError::SomeError
+        );
+        require_eq!(
+            base_bank.token_index,
+            openbook_market.base_token_index,
+            MangoError::SomeError
+        );
+    }
     //
     // close OO
     //
-    let account_seeds = mango_account_seeds!(account.fixed);
-    cpi_close_open_orders(ctx.accounts, &[account_seeds])?;
+    let group_key;
+    let owner_key;
+    let account_num;
+    let bump;
+    {
+        // this is so dumb how do i not do this
+        let account = ctx.accounts.account.load()?;
+        group_key = account.group;
+        owner_key = account.owner;
+        account_num = account.account_num;
+        bump = account.bump;
+    }
+    let seeds = &[
+        b"MangoAccount".as_ref(),
+        group_key.as_ref(),
+        owner_key.as_ref(),
+        &account_num.to_le_bytes(),
+        &[bump],
+    ];
+    cpi_close_open_orders(ctx.accounts, &[seeds])?;
 
     // Reduce the in_use_count on the token positions - they no longer need to be forced open.
     // Also dust the position since we have banks now
     let now_ts: u64 = Clock::get().unwrap().unix_timestamp.try_into().unwrap();
     let account_pubkey = ctx.accounts.account.key();
+    let mut account = ctx.accounts.account.load_full_mut()?;
+    let mut quote_bank = ctx.accounts.quote_bank.load_mut()?;
+    let mut base_bank = ctx.accounts.base_bank.load_mut()?;
     account.token_decrement_dust_deactivate(&mut quote_bank, now_ts, account_pubkey)?;
     account.token_decrement_dust_deactivate(&mut base_bank, now_ts, account_pubkey)?;
 
@@ -64,12 +86,11 @@ pub fn openbook_v2_close_open_orders(ctx: Context<OpenbookV2CloseOpenOrders>) ->
 }
 
 fn cpi_close_open_orders(ctx: &OpenbookV2CloseOpenOrders, seeds: &[&[&[u8]]]) -> Result<()> {
-    let group = ctx.group.load()?;
     let cpi_accounts = CloseOpenOrdersAccount {
         owner: ctx.account.to_account_info(),
         open_orders_indexer: ctx.open_orders_indexer.to_account_info(),
         open_orders_account: ctx.open_orders_account.to_account_info(),
-        sol_destination: ctx.authority.to_account_info(),
+        sol_destination: ctx.sol_destination.to_account_info(),
         system_program: ctx.system_program.to_account_info(),
     };
 
@@ -86,7 +107,7 @@ fn cpi_close_open_orders(ctx: &OpenbookV2CloseOpenOrders, seeds: &[&[&[u8]]]) ->
         let cpi_accounts = CloseOpenOrdersIndexer {
             owner: ctx.account.to_account_info(),
             open_orders_indexer: ctx.open_orders_indexer.to_account_info(),
-            sol_destination: ctx.authority.to_account_info(),
+            sol_destination: ctx.sol_destination.to_account_info(),
             token_program: ctx.token_program.to_account_info(),
         };
 
