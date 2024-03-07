5c429d79132a1c8cfd7e6128f469b9452eeaa39e
Christian Kamm <mail@ckamm.de>
2024-03-07 14:39:15 +0100

    token balance log v2

--- programs/mango-v4/src/logs.rs
+++ programs/mango-v4/src/logs.rs
@@ -55,13 +55,24 @@ pub struct PerpBalanceLog {
 
 pub fn emit_token_balance_log(mango_account: Pubkey, bank: &Bank, token_position: &TokenPosition) {
     assert_eq!(bank.token_index, token_position.token_index);
-    emit_stack(TokenBalanceLog {
+    let allow_lending = token_position.allow_lending();
+    emit_stack(TokenBalanceLogV2 {
         mango_group: bank.group,
         mango_account,
         token_index: bank.token_index,
-        indexed_position: token_position.indexed_position.to_bits(), // TODO ### oops, what if indexed_position is no-lending?
+        indexed_position: if allow_lending {
+            token_position.indexed_position.to_bits()
+        } else {
+            0
+        },
         deposit_index: bank.deposit_index.to_bits(),
         borrow_index: bank.borrow_index.to_bits(),
+        native_position: if !allow_lending {
+            token_position.indexed_position.to_num::<u64>()
+        } else {
+            0
+        },
+        allow_lending: token_position.allow_lending(),
     });
 }
 
@@ -73,6 +84,18 @@ pub struct TokenBalanceLog {
     pub indexed_position: i128, // on client convert i128 to I80F48 easily by passing in the BN to I80F48 ctor
     pub deposit_index: i128,    // I80F48
     pub borrow_index: i128,     // I80F48
+}
+
+#[event]
+pub struct TokenBalanceLogV2 {
+    pub mango_group: Pubkey,
+    pub mango_account: Pubkey,
+    pub token_index: u16,       // IDL doesn't support usize
+    pub indexed_position: i128, // on client convert i128 to I80F48 easily by passing in the BN to I80F48 ctor
+    pub deposit_index: i128,    // I80F48
+    pub borrow_index: i128,     // I80F48
+    pub native_position: u64,
+    pub allow_lending: bool,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
