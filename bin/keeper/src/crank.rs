f80b72d9e0548071371e2c6c3fef9c4df65ab46e
microwavedcola1 <microwavedcola@gmail.com>
2024-04-24 10:38:23 +0200

    keeper: dont crank closed tokens
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- bin/keeper/src/crank.rs
+++ bin/keeper/src/crank.rs
@@ -104,7 +104,9 @@ pub async fn runner(
     let handles1 = mango_client
         .context
         .tokens
-        .keys()
+        .values()
+        .filter(|t| t.closed == false)
+        .map(|t| &t.token_index)
         // TODO: grouping tokens whose oracle might have less confidencen e.g. ORCA with the rest, fails whole ix
         // TokenUpdateIndexAndRate is known to take max 71k cu
         // from cargo test-bpf local tests
