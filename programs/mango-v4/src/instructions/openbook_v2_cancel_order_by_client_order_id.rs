d0d8a54fafd854156b213437124e8caf6cc0456e
Britt Cyr <cyrbritt@gmail.com>
2024-06-05 11:03:02 -0400

    Consolidate shared code

--- programs/mango-v4/src/instructions/openbook_v2_cancel_order_by_client_order_id.rs
+++ programs/mango-v4/src/instructions/openbook_v2_cancel_order_by_client_order_id.rs
@@ -3,9 +3,7 @@ use anchor_lang::prelude::*;
 use openbook_v2::cpi::accounts::CancelOrder;
 
 use crate::error::*;
-use crate::logs::{emit_stack, OpenbookV2OpenOrdersBalanceLog};
-use crate::serum3_cpi::OpenOrdersAmounts;
-use crate::serum3_cpi::OpenOrdersSlim;
+use crate::instructions::{validate_openbook_v2_cancel_order, emit_openbook_v2_balance_log};
 use crate::state::*;
 
 use crate::accounts_ix::*;
@@ -28,28 +26,7 @@ pub fn openbook_v2_cancel_order_by_client_order_id(
 
     let openbook_market = ctx.accounts.openbook_v2_market.load()?;
 
-    //
-    // Validation
-    //
-    {
-        let account = ctx.accounts.account.load_full()?;
-        // account constraint #1
-        require!(
-            account
-                .fixed
-                .is_owner_or_delegate(ctx.accounts.authority.key()),
-            MangoError::SomeError
-        );
-
-        // Validate open_orders #2
-        require!(
-            account
-                .openbook_v2_orders(openbook_market.market_index)?
-                .open_orders
-                == ctx.accounts.open_orders.key(),
-            MangoError::SomeError
-        );
-    }
+    validate_openbook_v2_cancel_order(&ctx, &openbook_market)?;
 
     //
     // Cancel cpi
@@ -58,27 +35,7 @@ pub fn openbook_v2_cancel_order_by_client_order_id(
     let account_seeds = mango_account_seeds!(account);
     cpi_cancel_order_by_client_order_id(ctx.accounts, &[account_seeds], client_order_id)?;
 
-    let open_orders = ctx.accounts.open_orders.load()?;
-    let openbook_market_external = ctx.accounts.openbook_v2_market_external.load()?;
-    let after_oo = OpenOrdersSlim::from_oo_v2(
-        &open_orders,
-        openbook_market_external.base_lot_size.try_into().unwrap(),
-        openbook_market_external.quote_lot_size.try_into().unwrap(),
-    );
-
-    emit_stack(OpenbookV2OpenOrdersBalanceLog {
-        mango_group: ctx.accounts.group.key(),
-        mango_account: ctx.accounts.account.key(),
-        market_index: openbook_market.market_index,
-        base_token_index: openbook_market.base_token_index,
-        quote_token_index: openbook_market.quote_token_index,
-        base_total: after_oo.native_base_total(),
-        base_free: after_oo.native_base_free(),
-        quote_total: after_oo.native_quote_total(),
-        quote_free: after_oo.native_quote_free(),
-        referrer_rebates_accrued: after_oo.native_rebates(),
-    });
-
+    emit_openbook_v2_balance_log(&ctx, &openbook_market)?;
     Ok(())
 }
 
