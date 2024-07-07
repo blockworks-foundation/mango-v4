b02ec278ee4446323bd479784c8c6bfd6fa6b1ea
microwavedcola1 <microwavedcola@gmail.com>
2024-07-07 11:07:12 +0200

    update default
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- ts/client/scripts/sb-on-demand-crank.ts
+++ ts/client/scripts/sb-on-demand-crank.ts
@@ -33,7 +33,7 @@ const CLUSTER_URL =
 const USER_KEYPAIR =
   process.env.USER_KEYPAIR_OVERRIDE || process.env.MB_PAYER_KEYPAIR;
 const GROUP = process.env.GROUP_OVERRIDE || MANGO_V4_MAIN_GROUP.toBase58();
-const SLEEP_MS = Number(process.env.SLEEP_MS) || 100_000; // 100s
+const SLEEP_MS = Number(process.env.SLEEP_MS) || 50_000; // 100s
 
 // TODO use mangolana to send txs
 
