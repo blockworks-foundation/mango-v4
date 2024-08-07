a3a9d44e890f8c2d6b18c5639f73dfe63897570a
microwavedcola1 <microwavedcola@gmail.com>
2024-08-07 11:56:13 +0200

    sb crank: also consider fallback oracles
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- ts/client/scripts/sb-on-demand-crank.ts
+++ ts/client/scripts/sb-on-demand-crank.ts
@@ -310,10 +310,7 @@ async function preparePullIx(
     gateway: oracle.gatewayUrl,
   };
   // TODO use fetchUpdateMany
-  const [pullIx] = await pullFeed.fetchUpdateIx(
-    conf,
-    recentSlothashes,
-  );
+  const [pullIx] = await pullFeed.fetchUpdateIx(conf, recentSlothashes);
 
   return pullIx;
 }
@@ -569,6 +566,22 @@ function getOraclesForMangoGroup(
       };
     });
 
+  const oraclesFb = Array.from(group.banksMapByName.values())
+    .filter(
+      (b) =>
+        !(
+          b[0].nativeDeposits().eq(ZERO_I80F48()) &&
+          b[0].nativeBorrows().eq(ZERO_I80F48()) &&
+          b[0].reduceOnly == 1
+        ),
+    )
+    .map((b) => {
+      return {
+        oraclePk: b[0].fallbackOracle,
+        name: b[0].name,
+      };
+    });
+
   // oracles for perp markets
   const oracles2 = Array.from(group.perpMarketsMapByName.values()).map((pM) => {
     return {
@@ -596,7 +609,7 @@ function getOraclesForMangoGroup(
       };
     })
     .filter((item) => !item.oraclePk.equals(PublicKey.default));
-  const oracles = oracles1.concat(oracles2).concat(oracles3);
+  const oracles = oracles1.concat(oraclesFb).concat(oracles2).concat(oracles3);
   return oracles;
 }
 
