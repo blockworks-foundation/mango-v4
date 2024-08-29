70fc0921f558ccd76438515ea2d51c1653636f95
Serge Farny <serge.farny@gmail.com>
2024-08-29 07:00:47 +0100

    Liquidator: fix rebalancing with sequence check (#999)

--- lib/client/src/client.rs
+++ lib/client/src/client.rs
@@ -2600,7 +2600,7 @@ impl TransactionBuilder {
 
     pub fn append(&mut self, prepared_instructions: PreparedInstructions) {
         self.instructions
-            .extend(prepared_instructions.to_instructions());
+            .extend(prepared_instructions.instructions);
     }
 }
 
