24af94c10cfc1ce59c6a1d1f69035ba2940a79ac
Adrian Brzeziński <a.brzezinski94@gmail.com>
2024-10-23 19:55:20 +0200

    fix

--- ts/client/scripts/reimbursement.ts
+++ ts/client/scripts/reimbursement.ts
@@ -2,8 +2,8 @@ import { Connection, Keypair, PublicKey } from '@solana/web3.js';
 import fs from 'fs';
 import * as path from 'path';
 import { parse } from 'csv-parse';
-import { AnchorProvider, Wallet } from 'switchboard-anchor';
-import { MANGO_V4_ID, MangoClient, USDC_MINT } from '../src';
+import { AnchorProvider, Wallet } from '@coral-xyz/anchor';
+import { MANGO_V4_ID, MangoClient, toNative, USDC_MINT } from '../src';
 import { WRAPPED_SOL_MINT } from '@project-serum/serum/lib/token-instructions';
 
 const MANGO_MAINNET_GROUP = new PublicKey(
@@ -17,94 +17,6 @@ type Reimbursement = {
   SOL: number;
   USDC: number;
   Notional: string;
-};
-
-const mints = {
-  mangoSOL: new PublicKey('MangmsBgFqJhW4cLUR9LxfVgMboY1xAoP8UUBiWwwuY'),
-  MOTHER: new PublicKey('3S8qX1MsMqRbiwKg2cQyx7nis1oHMgaCuc9c4VfvVdPN'),
-  SOL: WRAPPED_SOL_MINT,
-  USDC: USDC_MINT,
-};
-
-const main = async () => {
-  const user = await setupWallet();
-  const mainConnection = new Connection('');
-  const backupConnections = [new Connection(''), new Connection('')];
-  const options = AnchorProvider.defaultOptions();
-  const userWallet = new Wallet(user);
-  const userProvider = new AnchorProvider(mainConnection, userWallet, options);
-  const client = await MangoClient.connect(
-    userProvider,
-    'mainnet-beta',
-    MANGO_V4_ID['mainnet-beta'],
-    {
-      idsSource: 'api',
-      multipleConnections: backupConnections,
-      prioritizationFee: 200000,
-    },
-  );
-
-  const group = await client.getGroup(MANGO_MAINNET_GROUP);
-
-  const csvData = await readCsv();
-
-  const TO_PROCESS = csvData.slice(1, 5);
-  const TOKEN = 'SOL';
-
-  const notReimbursedMangoAccounts: string[] = [];
-  for (const row of TO_PROCESS) {
-    const mangoAccountPk = tryGetPubKey(row.mango_account);
-    if (mangoAccountPk) {
-      try {
-        const mint = mints[TOKEN as keyof typeof mints];
-        const amount = Number(row[TOKEN as keyof typeof mints]);
-        if (mint && amount > 0) {
-          const mangoAccount = await client.getMangoAccount(mangoAccountPk);
-          console.log('Mango Account exists');
-          console.log(
-            `Start reimbursing ${mint.toBase58()} ${amount} ${
-              row.mango_account
-            }`,
-          );
-          try {
-            const signature = await client.tokenDeposit(
-              group,
-              mangoAccount,
-              mint,
-              amount,
-            );
-            console.log(
-              'Reimburse end ',
-              signature.signature,
-              signature.confirmationStatus,
-              signature.err,
-            );
-            if (signature.confirmationStatus === 'confirmed') {
-              console.log('OK');
-            } else {
-              notReimbursedMangoAccounts.push(row.mango_account);
-            }
-          } catch (e) {
-            console.log(e);
-            notReimbursedMangoAccounts.push(row.mango_account);
-          }
-        }
-      } catch (e) {
-        console.log('Mango account not exists');
-        const wallet = await ownerOfMangoAccount(row.mango_account);
-        if (!wallet) {
-          notReimbursedMangoAccounts.push(row.mango_account);
-        } else {
-          notReimbursedMangoAccounts.push(row.mango_account);
-          console.log('Mango Account: ', row.mango_account, 'Owner: ', wallet);
-        }
-      }
-    } else {
-      console.log('Invalid PublicKey: ', row.mango_account);
-      throw 'Invalid PublicKey';
-    }
-  }
-  console.log(notReimbursedMangoAccounts);
 };
 
 const setupWallet = () => {
@@ -133,6 +45,7 @@ const ownerOfMangoAccount = async (mangoAccount: string) => {
     }
     return accountOwner as string;
   } catch (e) {
+    console.log(e);
     console.log('cant find mangoAccount:', mangoAccount);
   }
 };
@@ -170,12 +83,114 @@ const readCsv = async () => {
   });
 };
 
-main();
-
-export const tryGetPubKey = (pubkey: string | string[]) => {
+const tryGetPubKey = (pubkey: string | string[]) => {
   try {
     return new PublicKey(pubkey);
   } catch (e) {
+    console.log(e);
     return null;
   }
 };
+
+const mints = {
+  mangoSOL: new PublicKey('MangmsBgFqJhW4cLUR9LxfVgMboY1xAoP8UUBiWwwuY'),
+  MOTHER: new PublicKey('3S8qX1MsMqRbiwKg2cQyx7nis1oHMgaCuc9c4VfvVdPN'),
+  SOL: WRAPPED_SOL_MINT,
+  USDC: USDC_MINT,
+};
+
+const main = async () => {
+  const user = await setupWallet();
+  const mainConnection = new Connection('http://fcs-da1._peer.internal:18899/');
+  const backupConnections = [
+    new Connection(
+      'https://staked.helius-rpc.com?api-key=c367d0d9-acf2-437d-9ef3-72304f1a4117',
+    ),
+    new Connection('https://rpc.mngo.cloud/adr9quiemae0'),
+  ];
+  const options = AnchorProvider.defaultOptions();
+  const userWallet = new Wallet(user);
+  const userProvider = new AnchorProvider(mainConnection, userWallet, options);
+
+  const client = await MangoClient.connect(
+    userProvider,
+    'mainnet-beta',
+    MANGO_V4_ID['mainnet-beta'],
+    {
+      idsSource: 'api',
+      multipleConnections: backupConnections,
+      prioritizationFee: 200000,
+    },
+  );
+  console.log(userWallet.publicKey.toBase58(), '@@@@@');
+  const group = await client.getGroup(MANGO_MAINNET_GROUP);
+
+  const csvData = await readCsv();
+
+  const TO_PROCESS = csvData;
+  const TOKEN = 'MOTHER';
+
+  const notReimbursedMangoAccounts: string[] = [];
+  for (const row of TO_PROCESS) {
+    const mangoAccountPk = tryGetPubKey(row.mango_account);
+
+    if (mangoAccountPk) {
+      try {
+        const mint = mints[TOKEN as keyof typeof mints];
+        const amount = Number(row[TOKEN as keyof typeof mints]);
+        if (mint && amount > 0) {
+          const mangoAccount = await client.getMangoAccount(mangoAccountPk);
+          console.log('Mango Account exists');
+          console.log(
+            `Start reimbursing ${mint.toBase58()} ${amount} ${
+              row.mango_account
+            }`,
+          );
+          try {
+            const decimals = group.getMintDecimals(mint);
+            const nativeAmount = toNative(amount, decimals);
+
+            const signature = await client.tokenDepositNative(
+              group,
+              mangoAccount,
+              mint,
+              nativeAmount,
+              false,
+              true,
+            );
+
+            console.log(
+              'Reimburse end ',
+              signature.signature,
+              signature.confirmationStatus,
+              signature.err,
+            );
+            if (!signature.err) {
+              console.log('OK');
+            } else {
+              notReimbursedMangoAccounts.push(row.mango_account);
+            }
+          } catch (e) {
+            console.log(e);
+            notReimbursedMangoAccounts.push(row.mango_account);
+          }
+        }
+      } catch (e) {
+        console.log('Mango account not exists', e);
+        const wallet = await ownerOfMangoAccount(row.mango_account);
+        if (!wallet) {
+          notReimbursedMangoAccounts.push(row.mango_account);
+        } else {
+          notReimbursedMangoAccounts.push(row.mango_account);
+          console.log('Mango Account: ', row.mango_account, 'Owner: ', wallet);
+        }
+      }
+    } else {
+      console.log('Invalid PublicKey: ', row.mango_account);
+      throw 'Invalid PublicKey';
+    }
+  }
+  console.log(notReimbursedMangoAccounts);
+};
+
+main();
